f430fdabc5421176a4c4cc7b6ce9cc37543555c5
mai57 <krisaday57@email.nu.ac.th>
2019-01-16 00:03:00 +0700

    updatereg

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -38,11 +38,59 @@ export class RegisterPage {
     this.user.u_Email = this.navParams.get("u_Email");
     this.user.u_Password = this.navParams.get("u_Password");
     this.confirmPassword = this.navParams.get("u_Password");
+    this.user.u_Tel = " ";
+    this.user.u_Gender = " ";
+    this.user.u_Address = " ";
   }
 
   async save() {
-    if (this.user.u_Password != this.confirmPassword) {
-      const alert = this.alertCtrl.create({
+    
+    // let prompt = this.alertCtrl.create({
+    //   title: 'ข้อมูล',
+    //   message: "คุณต้องการแก้ไขที่อยู่หรือไม่",
+    //   buttons: [
+    //     {
+    //       text: 'Cancel',
+    //       handler: data => {
+    //         console.log('Cancel clicked');
+    //       }
+    //     },
+    //     {
+    //       text: 'Save',
+    //       handler: data => {
+    //         console.log('Saved clicked');
+    //         this.cartService.updateUser(Number(this.user.u_Id), this.user).subscribe();
+    //         this.navCtrl.pop();
+    //       }
+    //     }
+    //   ]
+    // });
+    // prompt.present();
+
+    
+    if (this.user.u_Name == undefined) {
+      let alert = this.alertCtrl.create({
+        title: 'Error..',
+        subTitle: 'กรุณาใส่ชื่อ-นามสกุล ',
+        buttons: ['OK']
+      });
+      alert.present();
+    }else if (this.user.u_Email == undefined) {
+      let alert = this.alertCtrl.create({
+        title: 'Error..',
+        subTitle: 'กรุณาใส่ Email ',
+        buttons: ['OK']
+      });
+      alert.present();
+    }else if (this.user.u_Password == undefined) {
+      let alert = this.alertCtrl.create({
+        title: 'Error..',
+        subTitle: 'กรุณาใส่รหัสผ่าน ',
+        buttons: ['OK']
+      });
+      alert.present();
+    }else if (this.user.u_Password != this.confirmPassword) {
+      let alert = this.alertCtrl.create({
         title: 'Error..',
         subTitle: 'รหัสผ่านไม่ตรงกัน โปรดลองอีกครั้ง',
         buttons: ['OK']
@@ -61,7 +109,7 @@ export class RegisterPage {
       });
     }
 
-    // console.log(this.user);
+    console.log(this.user);
   }
 
   test() {
